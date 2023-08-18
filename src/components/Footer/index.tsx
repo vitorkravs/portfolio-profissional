17bec288dc14fa0e661bf6cdaaf1f5cf541510c9
Vitor Kravszenko <vitorkravs3@gmail.com>
2023-08-18 00:17:01 -0300

    att

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,30 +1,44 @@
 'use client'
-import { Container } from './styles';
+import { Container } from './styles'
 
 import { AiFillInstagram, AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 export function Footer() {
-  function handleRedirect(url: string){
+  function handleRedirect(url: string) {
     window.open(url)
   }
 
   function handleScrollTop() {
     window.scroll({
       top: 0,
-      behavior: 'smooth'
-    });
+      behavior: 'smooth',
+    })
   }
 
   return (
     <Container>
-      <div className='container'>
-        <button type='button' onClick={handleScrollTop}>Voltar ao topo</button>
+      <div className="container">
+        <button type="button" onClick={handleScrollTop}>
+          Voltar ao topo
+        </button>
         <section>
-          <AiFillInstagram onClick={() => handleRedirect("https://www.instagram.com/vitorkravs")}/>
-          <AiFillGithub onClick={() => handleRedirect("https://github.com/vitorkravs")}/>
-          <AiFillLinkedin onClick={() => handleRedirect("https://www.linkedin.com/in/vitor-kravszenko-80748a234/")}/>
+          <AiFillInstagram
+            onClick={() =>
+              handleRedirect('https://www.instagram.com/vitorkravs')
+            }
+          />
+          <AiFillGithub
+            onClick={() => handleRedirect('https://github.com/vitorkravs')}
+          />
+          <AiFillLinkedin
+            onClick={() =>
+              handleRedirect(
+                'https://www.linkedin.com/in/vitor-kravszenko-80748a234/',
+              )
+            }
+          />
         </section>
       </div>
     </Container>
-  );
+  )
 }
